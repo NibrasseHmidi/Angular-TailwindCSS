0f3ebaf3c5236550aaef0728b476f9607f02f950
NibrasseHmidi <nibrasse.hmidi@gmail.com>
2022-02-11 19:11:18 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do-app';
+  title = 'SMALL BUISNESS';
+//   list:any[]=[];
+//   addTask(item:string){
+// this.list.push({id:this.list.length,name:item})
+// console.log(this.list)
+
+//   }
+//   removeTask(id:number){
+//     console.log(id)
+//     this.list=this.list.filter(item=>item.id!==id)
+//   }
+
 }
---
+++ src/app/models/Todo.ts
@@ -0,0 +1,7 @@
+export class Todo {
+  constructor(
+   public content:string,
+  public completed:boolean,
+  ) {}
+
+}
